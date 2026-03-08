2588a7659033fe5a1a886c068989e7d8f64d653a
Saurav Shrestha <saurastha@gmail.com>
2026-03-08 11:45:16 +1100

    fix test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,4 +8,4 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello from ML Ops!"}
+    assert response.json() == {"message": "Hello from MLOps!"}
